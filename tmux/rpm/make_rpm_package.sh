0b828a1b1510e15f34a26dec3401893e9650fc9f
Cloud User <centos@ip-172-31-18-158.ap-northeast-1.compute.internal>
2023-03-11 10:11:33 +0000

    add tmux rpm

---
+++ tmux/rpm/make_rpm_package.sh
@@ -0,0 +1,67 @@
+#!/bin/bash -e
+
+if [ $# -ne 2 ]; then
+  echo "specify tmux verion and arch!"
+  echo "ex: ./make_deb_package.sh 3.3 amd64"
+  echo "ex: ./make_deb_package.sh 3.3 arm64"
+  exit 1
+fi
+TMUX_VERSION=$1
+DEB_ARCH=$2
+
+# Cleanup
+WORK_DIR=work-dir
+if [[ -d $WORK_DIR ]]; then
+    rm -rf $WORK_DIR
+fi
+mkdir work-dir
+cd $WORK_DIR
+
+# Build tmux
+sudo yum install -y rpm-build automake gcc byacc libevent-devel libevent ncurses-devel ncurses
+git clone https://github.com/tmux/tmux.git
+cd tmux
+git checkout "${TMUX_VERSION}"
+sh autogen.sh
+./configure
+make
+cd ..
+
+# Prepare files
+BUILDDIR=$(pwd)/buildroot
+mkdir -p $BUILDDIR/SOURCES
+cp tmux/tmux   $BUILDDIR/SOURCES/
+cp tmux/tmux.1 $BUILDDIR/SOURCES/
+
+# Set params
+VERSION=$TMUX_VERSION.$(date "+%Y.%m.%d.%H.%M")
+SPEC=tmux.spec
+cat << EOS > ./$SPEC
+Name:    tmux
+Version: ${TMUX_VERSION}
+Release: ${VERSION}%{?dist}
+Summary: tmux ${TMUX_VERSION} for my own build
+
+License: ISC license
+Source0: tmux
+Source1: tmux.1
+
+%description
+tmux ${TMUX_VERSION} for my own build
+
+%install
+mkdir -p %{buildroot}/usr/local/bin
+install -p -m 755 %{SOURCE0} %{buildroot}/usr/local/bin
+
+
+mkdir -p %{buildroot}/usr/local/share/man/man1/
+install -p -m 755 %{SOURCE1} %{buildroot}/usr/local/share/man/man1/
+
+%files
+/usr/local/bin/tmux
+/usr/local/share/man/man1/tmux.1
+EOS
+
+# Build deb package
+rpmbuild --define "_topdir ${BUILDDIR}" -bb ./$SPEC
+cp $BUILDDIR/RPMS/x86_64/*.rpm .
