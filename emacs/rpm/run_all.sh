4bc0622e715cdd2e8c1cebd3189cba8a422af29c
cafenero <2006yusuke@gmail.com>
2023-03-13 17:36:53 +0000

    fix docker args

--- emacs/rpm/run_all.sh
+++ emacs/rpm/run_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker compose build --build-arg EMACS_VERSION=$1 ARCH=$2
+docker compose build --build-arg EMACS_VERSION="$1" --build-arg ARCH="$2"
 docker compose up -d
 
-RPM_PATH=$(docker exec tmux-rpm find ./work-dir/ | grep "work-dir/emacs.*rpm")
+RPM_PATH=$(docker exec emacs-rpm find ./work-dir/ | grep "work-dir/emacs.*rpm$")
 docker cp emacs-rpm:/root/"$RPM_PATH" .
