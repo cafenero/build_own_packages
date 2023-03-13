40db151143d690890360230ba006968464795179
cafenero <2006yusuke@gmail.com>
2023-03-13 17:38:23 +0000

    cleanup script

--- tmux/rpm/run_all.sh
+++ tmux/rpm/run_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker compose build --build-arg TMUX_VERSION=$1 --build-arg ARCH=$2
+docker compose build --build-arg TMUX_VERSION="$1" --build-arg ARCH="$2"
 docker compose up -d
 
 RPM_PATH=$(docker exec tmux-rpm find ./work-dir/ | grep "work-dir/tmux.*rpm")
