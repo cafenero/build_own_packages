04d3570359fc6f310649c90b1103c431531fad17
cafenero <2006yusuke@gmail.com>
2023-03-13 17:37:42 +0000

    fix build: add /etc/charsets files

--- emacs/rpm/make_pkg.sh
+++ emacs/rpm/make_pkg.sh
@@ -42,6 +42,7 @@ cp emacs/lib-src/etags       "$BUILDDIR"/SOURCES/
 cp emacs/src/emacs           "$BUILDDIR"/SOURCES/
 cp -r emacs/lisp             "$BUILDDIR"/SOURCES/
 cp -r emacs/doc/man/*        "$BUILDDIR"/SOURCES/
+cp -r emacs/etc/             "$BUILDDIR"/SOURCES/
 
 # Set params
 VERSION=$EMACS_VERSION.$(date "+%Y.%m.%d.%H.%M")
@@ -69,35 +70,43 @@ emacs ${EMACS_VERSION} for my own build
 
 %install
 mkdir -p %{buildroot}/usr/local/bin
+mkdir -p %{buildroot}/usr/local/share/man/man1
+mkdir -p %{buildroot}/usr/local/share/emacs/${EMACS_VERSION}/etc/charsets
 install -p -m 755 %{SOURCE0} %{buildroot}/usr/local/bin
 install -p -m 755 %{SOURCE1} %{buildroot}/usr/local/bin
 install -p -m 755 %{SOURCE2} %{buildroot}/usr/local/bin
 install -p -m 755 %{SOURCE3} %{buildroot}/usr/local/bin
 install -p -m 755 %{SOURCE4} %{buildroot}/usr/local/bin
-mkdir -p %{buildroot}/usr/local/share/man/man1
 install -p -m 755 %{SOURCE5} %{buildroot}/usr/local/share/man/man1
 install -p -m 755 %{SOURCE6} %{buildroot}/usr/local/share/man/man1
 install -p -m 755 %{SOURCE7} %{buildroot}/usr/local/share/man/man1
 install -p -m 755 %{SOURCE8} %{buildroot}/usr/local/share/man/man1
 install -p -m 755 %{SOURCE9} %{buildroot}/usr/local/share/man/man1
 
-
 if [[ $RPM_ARCH == "arm64" ]]; then
   mkdir -p %{buildroot}/usr/local/libexec/emacs/${EMACS_VERSION}/aarch64-unknown-linux-gnu/
 else
-  mkdir -p %{buildroot}/usr/local/libexec/emacs/${EMACS_VERSION}/x86-unknown-linux-gnu/
+  mkdir -p %{buildroot}/usr/local/libexec/emacs/${EMACS_VERSION}/x86_64-pc-linux-gnu/
 fi
+EOS
 
 # 仮想インストール先にlisp用のディレクトリを先に作っておく。
-for f in $(find "$BUILDDIR"/SOURCES/lisp -type d | sed -e s/.*SOURCES// | xargs); do
-    mkdir -p %{buildroot}/usr/local/share/emacs/${EMACS_VERSION}/\$f
+for f in $(find "$BUILDDIR"/SOURCES/lisp -type d | sed -e s/.*SOURCES\\/// | xargs); do
+    echo mkdir -p "%{buildroot}/usr/local/share/emacs/${EMACS_VERSION}/$f" >> ./$SPEC
 done
 
 # 仮想インストール先にlispファイルをinstallする。
-for f in $(find "$BUILDDIR"/SOURCES/lisp -type f | sed -e s/.*SOURCES// | xargs); do
-    install -p -m 755 $BUILDDIR/SOURCES/\$f %{buildroot}/usr/local/share/emacs/${EMACS_VERSION}/\$f
+for f in $(find "$BUILDDIR"/SOURCES/lisp -type f | sed -e s/.*SOURCES\\/// | xargs); do
+    echo install -p -m 755 "$BUILDDIR/SOURCES/$f %{buildroot}/usr/local/share/emacs/${EMACS_VERSION}/$f"  >> ./$SPEC
 done
 
+# charsets el
+for f in $(find "$BUILDDIR"/SOURCES/etc/charsets/ -type f -printf '%f\n' | xargs); do
+    echo install -p -m 755 "$BUILDDIR/SOURCES/etc/charsets/$f %{buildroot}/usr/local/share/emacs/${EMACS_VERSION}/etc/charsets/$f"  >> ./$SPEC
+done
+
+
+cat << EOS >> ./$SPEC
 %files
 /usr/local/bin/ctags
 /usr/local/bin/ebrowse
@@ -110,9 +119,7 @@ done
 /usr/local/share/man/man1/emacsclient.1
 /usr/local/share/man/man1/etags.1
 /usr/local/libexec/emacs/*
-# lispファイルはたくさんある！
 /usr/local/share/emacs/*
-
 EOS
 
 # Build deb package
